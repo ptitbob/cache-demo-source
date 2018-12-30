8ece8946d93562d75a678bbc70a213e9ee59765b
ptitbob <github@shipstone.org>
2018-12-30 01:17:13 +0100

    app: mise en place de swagger

---
+++ app/src/main/java/org/shipstone/demo/cache/app/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package org.shipstone.demo.cache.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Demo de l'utilisation du cache en envirronement Spring
+ *
+ * Mise à disposition du fichier descriptiof swagger
+ *
+ * @author François Robert
+ * LICENCE Apache 2.0
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build();
+  }
+
+}
